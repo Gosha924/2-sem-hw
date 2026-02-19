aa10628ead685a1593b738915fcbccf4eeb2d905
Gosha924 <151758180+Gosha924@users.noreply.github.com>
2026-02-19 20:17:25 +0300

    check auto clang-format in vs code

--- src/test.c
+++ src/test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main(void)
-{
+void printNum(int num) {
+    printf("num is %d\n", num);
+}
+
+int main(void){ 
     printf("test\n");
     printf("test2\n");
+    printNum(21);
     return 0;
 }
