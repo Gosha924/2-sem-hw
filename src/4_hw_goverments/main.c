4b920a57b7b19e64d370e523f441f971cb0ec1f2
Gosha924 <151758180+Gosha924@users.noreply.github.com>
2026-04-11 22:58:12 +0300

    update main.c

--- src/4_hw_goverments/main.c
+++ src/4_hw_goverments/main.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* distribute(int countCity, Node** listSmev, int* capitals, int countCapitals)
+int* distribute(int countCity, const Node** listSmev, int* capitals, int countCapitals)
 {
     int* result = (int*)malloc((countCity + 1) * sizeof(int));
     if (result == NULL) {
-        printf("Error can not allacote memory\n");
+        printf("Error: cannot allocate memory\n");
         return NULL;
     }
     for (int i = 1; i <= countCity; i++) {
@@ -47,49 +47,69 @@ int* distribute(int countCity, Node** listSmev, int* capitals, int countCapitals
 
 int main(int arg, char* argv[])
 {
-    // открытие файла
+    // проверка аргументов командной строки
     if (arg < 2) {
         printf("Input file is not found\n");
         return 1;
     }
-
+    // чтение из файла
     char* filename = argv[1];
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("File could not be opened\n");
         return 1;
     }
-    // чтение из файла
+
     int countCity, countRoads;
-    fscanf(file, "%d", &countCity);
-    fscanf(file, "%d", &countRoads);
-    Node** listSmew = (Node**)calloc(countCity + 1, sizeof(Node*));
-    if (listSmew == NULL) {
-        printf("Error can not allacote memory\n");
+    if (fscanf(file, "%d", &countCity) != 1) {
+        printf("Error reading number of cities\n");
         fclose(file);
         return 1;
     }
+    if (fscanf(file, "%d", &countRoads) != 1) {
+        printf("Error reading number of roads\n");
+        fclose(file);
+        return 1;
+    }
+
+    Node** listSmew = (Node**)calloc(countCity + 1, sizeof(Node*));
+    if (listSmew == NULL) {
+        printf("Error: cannot allocate memory\n");
+        fclose(file);
+        return 1;
+    }
+
     for (int i = 0; i < countRoads; i++) {
         int city1, city2, len;
         if (fscanf(file, "%d %d %d", &city1, &city2, &len) != 3) {
-            printf("Eror wrong recoding in file string %d\n", i + 1);
+            printf("Error: wrong data format in road line %d\n", i + 1);
             freeGragh(listSmew, countCity);
             fclose(file);
             return 1;
         }
         addEdge(listSmew, city1, city2, len);
     }
+
     int countCapitals;
-    fscanf(file, "%d", &countCapitals);
-    int* capitals = (int*)malloc(countCapitals * sizeof(int));
-    if (capitals == NULL) {
-        printf("Error can not allacote memory\n");
+    if (fscanf(file, "%d", &countCapitals) != 1) {
+        printf("Error reading number of capitals\n");
+        freeGragh(listSmew, countCity);
+        fclose(file);
         return 1;
     }
+
+    int* capitals = (int*)malloc(countCapitals * sizeof(int));
+    if (capitals == NULL) {
+        printf("Error: cannot allocate memory for capitals\n");
+        freeGragh(listSmew, countCity);
+        fclose(file);
+        return 1;
+    }
+
     for (int i = 0; i < countCapitals; i++) {
         int capital;
         if (fscanf(file, "%d", &capital) != 1) {
-            printf("Eror reading capitals string %d\n", i + 1);
+            printf("Error reading capital at line %d\n", i + 1);
             free(capitals);
             freeGragh(listSmew, countCity);
             fclose(file);
@@ -99,7 +119,14 @@ int main(int arg, char* argv[])
     }
     fclose(file);
 
-    int* result = distribute(countCity, listSmew, capitals, countCapitals);
+    int* result = distribute(countCity, (const Node**)listSmew, capitals, countCapitals);
+    if (result == NULL) {
+        printf("Error during distribution\n");
+        free(capitals);
+        freeGragh(listSmew, countCity);
+        return 1;
+    }
+
     for (int s = 0; s < countCapitals; s++) {
         printf("Capital %d has cities:", capitals[s]);
         for (int city = 1; city <= countCity; city++) {
@@ -109,8 +136,9 @@ int main(int arg, char* argv[])
         }
         printf("\n");
     }
-    freeGragh(listSmew, countCity);
+
     free(result);
     free(capitals);
+    freeGragh(listSmew, countCity);
     return 0;
 }
